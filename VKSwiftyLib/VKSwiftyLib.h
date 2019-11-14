bb96dac87e7f3e18ee9785f5c89e36f1a4800ffd
Varun Kumar <varun>
2019-11-14 15:08:08 +0530

    Test commit for cocoa pod

---
+++ VKSwiftyLib/VKSwiftyLib.h
@@ -0,0 +1,19 @@
+//
+//  VKSwiftyLib.h
+//  VKSwiftyLib
+//
+//  Created by Varun Kumar on 14/11/19.
+//  Copyright © 2019 VK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VKSwiftyLib.
+FOUNDATION_EXPORT double VKSwiftyLibVersionNumber;
+
+//! Project version string for VKSwiftyLib.
+FOUNDATION_EXPORT const unsigned char VKSwiftyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VKSwiftyLib/PublicHeader.h>
+
+
---
+++ VKSwiftyLibTests/VKSwiftyLibTests.swift
@@ -0,0 +1,34 @@
+//
+//  VKSwiftyLibTests.swift
+//  VKSwiftyLibTests
+//
+//  Created by Varun Kumar on 14/11/19.
+//  Copyright © 2019 VK. All rights reserved.
+//
+
+import XCTest
+@testable import VKSwiftyLib
+
+class VKSwiftyLibTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
