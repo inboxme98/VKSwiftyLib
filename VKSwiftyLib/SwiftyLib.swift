ba532b1f9d01ff86a92db38bdded280bceb93ad4
Varun Kumar <varun>
2019-11-14 15:28:48 +0530

    test cases added

---
+++ VKSwiftyLib/SwiftyLib.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftyLib.swift
+//  VKSwiftyLib
+//
+//  Created by Varun Kumar on 14/11/19.
+//  Copyright © 2019 VK. All rights reserved.
+//
+
+public final class SwiftyLib {
+
+    let name = "VKSwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- VKSwiftyLibTests/VKSwiftyLibTests.swift
+++ VKSwiftyLibTests/VKSwiftyLibTests.swift
@@ -11,13 +11,25 @@ import XCTest
 
 class VKSwiftyLibTests: XCTestCase {
 
+    var swiftyLib: SwiftyLib!
+
     override func setUp() {
+        swiftyLib = SwiftyLib()
+
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
+    func testAdd() {
+           XCTAssertEqual(swiftyLib.add(a: 1, b: 1), 2)
+       }
 
+    func testSub() {
+          XCTAssertEqual(swiftyLib.sub(a: 2, b: 1), 1)
+      }
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
 
     func testExample() {
         // This is an example of a functional test case.
