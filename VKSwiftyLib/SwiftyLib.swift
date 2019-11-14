b8c7861de8899dbfe0d752ed88fb22e6055dc43e
Varun Kumar <varun>
2019-11-14 15:55:53 +0530

    test commit

--- VKSwiftyLib/SwiftyLib.swift
+++ VKSwiftyLib/SwiftyLib.swift
@@ -4,11 +4,12 @@
 //
 //  Created by Varun Kumar on 14/11/19.
 //  Copyright © 2019 VK. All rights reserved.
-//
+//Test commit
 
 public final class SwiftyLib {
 
     let name = "VKSwiftyLib"
+    
     
     public func add(a: Int, b: Int) -> Int {
         return a + b
